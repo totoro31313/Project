3049d59f8c8bfce0444ba38dd3e37a3d7927ef0a
totoro31313 <90670441+totoro31313@users.noreply.github.com>
2023-01-11 14:02:25 +0900

    메인타이틀복구
    player interacting box 생성중

--- Source/Project/ProjectCharacter.cpp
+++ Source/Project/ProjectCharacter.cpp
@@ -5,9 +5,11 @@
 #include "Camera/CameraComponent.h"
 #include "Components/CapsuleComponent.h"
 #include "Components/InputComponent.h"
+#include "Engine/Classes/Components/BoxComponent.h"
 #include "GameFramework/CharacterMovementComponent.h"
 #include "GameFramework/Controller.h"
 #include "GameFramework/SpringArmComponent.h"
+
 
 //////////////////////////////////////////////////////////////////////////
 // AProjectCharacter
@@ -45,6 +47,8 @@ AProjectCharacter::AProjectCharacter()
 
 	// Note: The skeletal mesh and anim blueprint references on the Mesh component (inherited from Character) 
 	// are set in the derived blueprint asset named MyCharacter (to avoid direct content references in C++)
+	InteractionBox = CreateDefaultSubobject<UBoxComponent>(TEXT("Interaction box"));
+	InteractionBox->SetupAttachment(RootComponent);
 }
 
 //////////////////////////////////////////////////////////////////////////
--- Source/Project/ProjectCharacter.h
+++ Source/Project/ProjectCharacter.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
+#include "Engine/Classes/Components/BoxComponent.h"
 #include "ProjectCharacter.generated.h"
 
 UCLASS(config=Game)
@@ -68,5 +69,11 @@ public:
 	FORCEINLINE class USpringArmComponent* GetCameraBoom() const { return CameraBoom; }
 	/** Returns FollowCamera subobject **/
 	FORCEINLINE class UCameraComponent* GetFollowCamera() const { return FollowCamera; }
+
+
+//플레이어 interact
+private:
+	UPROPERTY(EditAnywhere)
+	UBoxComponent* InteractionBox;
 };
 
